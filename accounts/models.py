30bfe7358e5959e57d540b6535e28daf38ed5124
IgnisDa <ignisda2001@gmail.com>
2021-01-21 20:40:19 +0530

    feat(accounts/models.py): add following-follower model

--- accounts/models.py
+++ accounts/models.py
@@ -28,3 +28,26 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return f"{self.full_name()}'s account"
+
+
+class UserFollowing(models.Model):
+
+    user_id = models.ForeignKey(
+        "accounts.CustomUser", related_name="following", on_delete=models.CASCADE
+    )
+    following_user_id = models.ForeignKey(
+        "accounts.CustomUser", related_name="followers", on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user_id", "following_user_id"], name="unique_followers"
+            )
+        ]
+
+        ordering = ["-created"]
+
+    def __str__(self):
+        f"{self.user_id} follows {self.following_user_id}"
