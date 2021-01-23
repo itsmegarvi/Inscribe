4bbef9c527dd8087617c2784483d0ca243348658
Garvit Shubham <mailme.garvit@gmail.com>
2021-01-23 13:37:09 +0530

    feat(notes\admin.py) improving listview

--- notes/admin.py
+++ notes/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.Note)
 class NoteAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'slug', 'hidden','created_at')
+    list_filter = ("hidden",)
+    search_fields = ['title', 'body', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(models.Note, NoteAdmin)
