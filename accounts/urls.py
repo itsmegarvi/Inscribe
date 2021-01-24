77e3a78409fcc13b72dc729b762f08c36bb1af93
Tanishka-Garg <tan27peace@gmail.com>
2021-01-24 19:31:32 +0530

    discover url

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path("register/", views.CustomUserCreateView.as_view(), name="register"),
     path("info/<int:pk>/", views.CustomUserDetailView.as_view(), name="info"),
+    path("discover/", views.CustomUserCreateView.as_view(), name="discover"),
 ]
