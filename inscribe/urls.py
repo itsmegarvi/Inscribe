3c3b95224998662031570005e45d86a8d610abe8
IgnisDa <ignisda2001@gmail.com>
2021-01-26 21:01:44 +0550

    feat(urls.py): add markdownx associated urls

--- inscribe/urls.py
+++ inscribe/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", include("home.urls")),
     path("notes/", include("notes.urls")),
     path("accounts/", include("accounts.urls")),
+    path("markdownx/", include("markdownx.urls")),
 ]
