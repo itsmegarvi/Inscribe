a77ee7a69b07ae3a2d780301f01cc00f98e74198
IgnisDa <ignisda2001@gmail.com>
2021-02-01 19:23:21 +0550

    feat(admin.py): register comment model

--- notes/admin.py
+++ notes/admin.py
@@ -3,15 +3,19 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'hidden','created_at')
+    list_display = ("title", "slug", "hidden", "created_at")
     list_filter = ("hidden",)
-    search_fields = ['title', 'body', 'slug']
-    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ["title", "body", "slug"]
+    prepopulated_fields = {"slug": ("title",)}
 
-admin.site.register(models.Note, NoteAdmin)
 
+@admin.register(models.Bookmark)
 class BookmarkAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(models.Bookmark, BookmarkAdmin)
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
