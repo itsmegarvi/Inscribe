c048a207e3fa574f79a39dab37af72a870281010
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-04 20:37:03 +0550

    feat(views.py): add views to handle password change

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,18 @@
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth import views as auth_views
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, ListView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    ListView,
+    TemplateView,
+    UpdateView,
+)
+from mixins import CustomLoginRequiredMixin
 from notes import models as notes_models
 
 from . import forms
@@ -36,3 +48,37 @@ class CustomUserDetailView(DetailView):
 
 class CustomUserDiscoverView(ListView):
     queryset = accounts_models.CustomUser.objects.all()[:10]
+
+
+class ProfileView(CustomLoginRequiredMixin, TemplateView):
+    template_name = "accounts/profile.html"
+
+
+class CustomUserUpdateView(CustomLoginRequiredMixin, UpdateView):
+    model = accounts_models.CustomUser
+    fields = ("first_name", "last_name", "username", "email")
+
+    def get_success_url(self, *args, **kwargs):
+        return reverse_lazy("accounts:info", kwargs={"pk": self.object.pk})
+
+
+@login_required
+def password_change_view(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, "Your password was updated successfully!")
+            return redirect(reverse("accounts:password-change-done"))
+        else:
+            messages.info(request, "Please correct the errors below ")
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, "accounts/password-change.html", {"form": form})
+
+
+class CustomUserPasswordChangeDoneView(
+    CustomLoginRequiredMixin, auth_views.PasswordChangeDoneView
+):
+    template_name = "accounts/password-change-done.html"
