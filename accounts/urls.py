cf32786e6aa81f4e2b85c688947362090c9dcc9a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-04 20:37:19 +0550

    feat(urls.py): add urls to handle password and profile update

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,16 @@ urlpatterns = [
     path("register/", views.CustomUserCreateView.as_view(), name="register"),
     path("info/<int:pk>/", views.CustomUserDetailView.as_view(), name="info"),
     path("discover/", views.CustomUserDiscoverView.as_view(), name="discover"),
+    path("profile/", views.ProfileView.as_view(), name="profile"),
+    path("update/<int:pk>/", views.CustomUserUpdateView.as_view(), name="update"),
+    path(
+        "password-change/",
+        views.password_change_view,
+        name="password-change",
+    ),
+    path(
+        "password-change-done/",
+        views.CustomUserPasswordChangeDoneView.as_view(),
+        name="password-change-done",
+    ),
 ]
