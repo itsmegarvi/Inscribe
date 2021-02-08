9507766a7a2840643cbcd22a2473ba40c28aa3f3
Manisha <manishareddy149@gmail.com>
2021-02-08 17:12:57 +0530

    detailview

--- notes/views.py
+++ notes/views.py
@@ -53,32 +53,50 @@ class NotesListView(ListView):
 #         context["bookmarks"] = models.Bookmark.objects.filter(note=note).count()
 #         return context
 
-def note_detail(request, slug):
-    template_name = 'notes/detail.html'
-    note = get_object_or_404(Note, slug=slug)
-    comments = note.comments.filter(note=note, active=True)
-    bookmarks = Note.bookmarks.filter(note=note).count()
-    new_comment = None
-    # Comment posted
-    if request.method == 'POST':
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
+# def note_detail(request, slug):
+#     template_name = 'notes/detail.html'
+#     note = get_object_or_404(note, slug=slug)
+#     comments = note.comments.filter(note=note, active=True)
+#     #bookmarks = Note.bookmarks.filter(note=note).count()
+#     new_comment = None
+#     # Comment posted
+#     if request.method == 'POST':
+#         comment_form = CommentForm(data=request.POST)
+#         if comment_form.is_valid():
 
-            # Create Comment object but don't save to database yet
-            new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
-            new_comment.note = note
-            # Save the comment to the database
-            new_comment.save()
+#             # Create Comment object but don't save to database yet
+#             new_comment = comment_form.save(commit=False)
+#             # Assign the current post to the comment
+#             new_comment.note = note
+#             # Save the comment to the database
+#             new_comment.save()
+#     else:
+#         comment_form = CommentForm()
+
+#     return render(request, template_name, {'note': note,
+#                                            'comments': comments,
+#                                            #'bookmarks': bookmarks,
+#                                            'new_comment': new_comment,
+#                                            'comment_form': comment_form})
+
+
+def NoteDetail(request,pk):
+    post = get_object_or_404(Note, pk=pk)
+    comments=models.Comment.objects.filter(note=note)
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST or None)
+        if comment_form.is_valid():
+            comment=comment_form.save(commit=False)
+            comment.note=note
+            comment.save()
     else:
         comment_form = CommentForm()
-
-    return render(request, template_name, {'note': note,
-                                           'comments': comments,
-                                           'bookmarks': bookmarks,
-                                           'new_comment': new_comment,
-                                           'comment_form': comment_form})
-
+    context={
+        'note':note,
+        'comments':comments,
+        'comment_form':comment_form,
+    }
+    return render(request, 'notes/detail.html', context)
 
 class PrivateListView(ListView):
     """ This view will handle displaying of private notes of the user. """
