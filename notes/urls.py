8483b17cb10aade6e24fa2af95d3d40f2282c74a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-10 15:29:32 +0550

    feat(urls.py): add route for note report

--- notes/urls.py
+++ notes/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "notes"
 
 urlpatterns = [
+    path("report/<int:pk>/", views.ReportView.as_view(), name="report"),
     path("create/", views.NoteCreateView.as_view(), name="create"),
     path("list/", views.NotesListView.as_view(), name="list"),
     path("private/<int:pk>/", views.PrivateListView.as_view(), name="private"),
