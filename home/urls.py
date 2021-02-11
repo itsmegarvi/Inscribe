54fc56a0123c4490a301b9ea4b83e9b61ae6dd57
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-11 22:43:17 +0550

    feat(urls.py): add routing for playground responses views

--- home/urls.py
+++ home/urls.py
@@ -8,4 +8,15 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("discover/", views.DiscoverView.as_view(), name="discover"),
     path("faq/", views.FAQView.as_view(), name="faq"),
+    path("playground/", views.PlayGroundView.as_view(), name="playground"),
+    path(
+        "playground-response/",
+        views.playground_response,
+        name="playground-response",
+    ),
+    path(
+        "playground-polarity-response/",
+        views.playground_polarity_response,
+        name="playground-polarity-response",
+    ),
 ]
