fbe4c55d628ea7c194d89d7ae3f4d19d8e07698a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-15 11:09:29 +0550

    feat(admin.py): register customuser following model

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,3 +6,8 @@ from . import models
 @admin.register(models.CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(models.CustomUserFollowing)
+class CustomUserFollowingAdmin(admin.ModelAdmin):
+    pass
